a80b51e52335e66fd27d0660f25ddae731e6e455
Nelly <nelly@Nellys.local>
2024-05-06 19:27:09 -0400

    Fixing first task

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,19 +5,18 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Modified this line to decrement n
+        n -= 1
     return result
 
-if len(sys.argv) != 2:
-    print("Usage: python script.py <number>")
-    sys.exit(1)
-
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("Please provide a valid integer as input.")
-    sys.exit(1)
-
-f = factorial(n)
-print(f)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./factorial.py <number>")
+        sys.exit(1)
+    
+    try:
+        f = factorial(int(sys.argv[1]))
+        print(f)
+    except ValueError:
+        print("Please provide a valid integer.")
+        sys.exit(1)
 
